1acff8efdc68fe70f228888f051d81cbdfa9d974
Sartaj alam <sartajalam16042002@gmail.com>
2025-01-22 17:02:04 +0530

    Variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,4 +1,20 @@
 const accountId = 122244;
-let accountEmails = "sartaj62@gmail.com"
+let accountEmail = "sartaj62@gmail.com"
 var accountPassword = "37900"
 accountCity = "Bihar";
+
+// accountId = 23 // constant variable same variables name not Allowed
+
+console.log(accountId)
+
+/* Prefer not to use var
+Becouse of isssue in block scope and functional scopeclear */
+
+accountId = 45445454545;
+accountEmail = "sa@gmail.com"
+accountPassword = "12121121211"
+accountCity = "Delhi"
+
+// console.log(accountPassword)
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+const accountId = 122244;
+// let accountEmail = "sartaj62@gmail.com"
+// var accountPassword = "37900"
+// accountCity = "Bihar";
