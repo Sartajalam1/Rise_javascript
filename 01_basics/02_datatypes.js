6848d79072f087dad7786054710a04f1af6347b8
Sartaj alam <sartajalam16042002@gmail.com>
2025-01-22 17:32:31 +0530

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3) // we are using nodejs, not browser
+
+console.log("Sartaj")
+
+let name = "Sartaj"
+let age = 22;
+let isloggedIn = false
+let state = "bihar"
+
+// Datatypes
+//  number => 2 to power 53
+// BigInt
+// string => " " 
+// boolean => true/false 
+//  null => standalone value 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);  // object
+
+
