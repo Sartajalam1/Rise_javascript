e056ad4349efdc2c7ac9a7eb83e8317cc2750a1a
Sartaj alam <sartajalam16042002@gmail.com>
2025-01-22 23:40:00 +0530

    conversion and capmrision

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -23,4 +23,46 @@ let someNumber = 33;
 
 let stringnumber = String(someNumber)
 // console.log(stringnumber);
-console.log(typeof stringnumber);
+// console.log(typeof stringnumber);
+
+// ************************** Operations **************************
+
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue)
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+
+let str1 = "Hello"
+let str2 = "Sartaj"
+
+let str3 = str1 + " " + str2;
+// console.log(str3)
+
+// console.log("1" +2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2");
+
+// console.log((3+4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+// console.log(num2);
+
+let gameCounter = 100
+gameCounter++;
+
+console.log(gameCounter);
