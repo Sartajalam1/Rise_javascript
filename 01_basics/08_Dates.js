1fc903a947015ba2b90effbe91e5f699436e34c4
Sartaj alam <sartajalam16042002@gmail.com>
2025-02-03 17:24:35 +0530

    Dates and Time

---
+++ 01_basics/08_Dates.js
@@ -0,0 +1,33 @@
+// Dates 
+ 
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof(myDate))
+
+// let myCreateDate = new Date(2025, 0, 3)
+// let myCreateDate = new Date(2025, 0, 2, 1, 9)
+// let myCreateDate = new Date("2025-01-3")
+let myCreateDate = new Date("3-02-2025")
+// console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+console.log(newDate.getTime());
+
+
+
+
+
+
+
+
+
