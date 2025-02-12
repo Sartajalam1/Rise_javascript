81565421da600c5bc79ad73071c80d162449844c
Sartaj alam <sartajalam16042002@gmail.com>
2025-02-12 14:18:28 +0530

    objects

--- 02_Basics/02_objects.js
+++ 02_Basics/02_objects.js
@@ -21,14 +21,20 @@ const user = {
 // console.log(user[mySym])
 
 user.email = "Sartaj20@chatgpt.com"
-Object.freeze(user)                  // freeze function use karne ke baad kuch access nahi hoga user object ka usek badd se stop ho jayega
+// Object.freeze(user)                  // freeze function use karne ke baad kuch access nahi hoga user object ka usek badd se stop ho jayega
 user.email = "sartajalam16@gmail.com"
 // console.log(user.email);
 
-user.greeting = function(){
-    console.log("Hello Js Sir");
-    
-}
+user.greeting = function () {
+    console.log("Hello Js User");
+};
+
+user.greetingTwo = function (){
+    console.log(`Hello Js User ${this.name}`);
+};
+console.log(user.greeting());
+console.log(user.greetingTwo());
+
 
 
 
